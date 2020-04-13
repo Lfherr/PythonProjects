0f96dd89c853eb0b720ca4eb56f8184ad2eb7436
Lfherr <54966326+Lfherr@users.noreply.github.com>
2020-04-13 08:09:05 -0500

    Update Mapit.py

--- Mapit.py
+++ Mapit.py
@@ -1,13 +1,17 @@
+""" Sets google maps to give directions to given address
+"""
+
 import sys
 import pyperclip
 import webbrowser
 
-sys.argv #collects argument from command line,  no () because it is a variable, not function
+sys.argv                #collects argument from command line
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 1:   #Excludes script name
     address = ' '.join(sys.argv[1:]) #join elements of list
 
-else:
+else:                   #Alternative is grab address from clipboard
     address = pyperclip.paste()
 
+#Opens google maps with address attached.
 webbrowser.open_new_tab('https://www.google.com/maps/place/' + address)
